e839ee6c2173e402a2c3e9f92e0b668fe4bb3224
Shirish Kawatkar <shirish@synerzip.com>
2018-06-08 18:59:46 +0530

    update site

--- e2e-wp-admin_test.py
+++ e2e-wp-admin_test.py
@@ -2,7 +2,7 @@ from selenium import webdriver
  
 driver = webdriver.Firefox()
 driver.implicitly_wait(30)
-driver.get("http://localhost:9090/wp-admin/setup-config.php")
+driver.get("http://localhost/wp-admin/setup-config.php")
 # print driver.title
 
 # Select language
