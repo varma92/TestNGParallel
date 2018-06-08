c5784f9fa23998b0640dead7f18f70b03780b181
Shirish Kawatkar <shirish@synerzip.com>
2018-06-08 19:06:18 +0530

    Added logs

--- e2e-wp-admin_test.py
+++ e2e-wp-admin_test.py
@@ -1,15 +1,19 @@
 from selenium import webdriver
- 
+
+# Initialize Driver
+print "# Initialize Driver"
 driver = webdriver.Firefox()
 driver.implicitly_wait(30)
 driver.get("http://localhost/wp-admin/setup-config.php")
 # print driver.title
 
 # Select language
+print "# Select language"
 driver.find_element_by_id("language-continue").click()
 
 
 # Configure database
+print "# Configure database"
 driver.find_elements_by_link_text("Let's go!")[0].click()
 driver.find_element_by_id("uname").clear()
 driver.find_element_by_id("uname").send_keys("wordpress")
@@ -18,6 +22,7 @@ driver.find_element_by_id("pwd").send_keys("admin123")
 driver.find_element_by_name("submit").click()
 
 # Run Installation
+print "# Run Installation"
 driver.find_elements_by_link_text("Run the installation")[0].click()
 # driver.find_element_by_id("weblog_title").clear()
 driver.find_element_by_id("weblog_title").send_keys("Data-Storage")
@@ -31,6 +36,7 @@ driver.find_element_by_id("admin_email").send_keys("admin@email.com")
 driver.find_element_by_id("submit").click()
 
 # Login
+print "# Login"
 driver.find_elements_by_link_text("Log In")[0].click()
 driver.find_element_by_id("user_login").clear()
 driver.find_element_by_id("user_login").send_keys("purestorage")
@@ -39,4 +45,5 @@ driver.find_element_by_id("user_pass").send_keys("Pure$torage123")
 driver.find_element_by_id("wp-submit").click()
 
 # driver.quit()
+print "# driver.quit()"
 driver.quit()
